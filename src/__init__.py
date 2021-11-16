4199851fb05a209621b5a8cd160c4ae6d8d7f793
ygong <yuangongfdu@gmail.com>
2021-11-16 17:38:19 -0500

    after reformat

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 11/16/21 5:25 PM
-# @Author  : Yuan Gong
-# @Affiliation  : Massachusetts Institute of Technology
-# @Email   : yuangong@mit.edu
-# @File    : __init__.py.py
