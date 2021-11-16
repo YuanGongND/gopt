ad2562b1762482694fc1182dedee0d20d96e5459
ygong <yuangongfdu@gmail.com>
2021-11-15 22:38:37 -0500

    remove mac temps

--- src/prep_data/gen_seq_data_phn.py
+++ src/prep_data/gen_seq_data_phn.py
@@ -33,12 +33,13 @@ def process_feat_seq(feat, keys, labels, phn_dict):
         cur_key = keys[i].split('.')[0]
         key_set.append(cur_key)
 
+    feat_dim = feat.shape[1] - 1
+
     utt_cnt = len(list(set(key_set)))
     print('In total utterance number : ' + str(utt_cnt))
 
     # Pad all sequence to 50 because the longest sequence of the so762 dataset is shorter than 50.
-    # 84 is the dimension of the GoP feature.
-    seq_feat = np.zeros([utt_cnt, 50, 84])
+    seq_feat = np.zeros([utt_cnt, 50, feat_dim])
     # -1 means n/a, padded token
     # [utt, seq_len, 0] is the phone label, and the [utt, seq_len, 1] is the score label
     seq_label = np.zeros([utt_cnt, 50, 2]) - 1
@@ -74,30 +75,7 @@ def gen_phn_dict(label):
     return phn_dict
 
 # sequence training data
-tr_feat = load_feat('../so762_datafiles_new/yuan_tr_feats_phn_1.0.csv')
-tr_keys = load_keys('../so762_datafiles_new/yuan_tr_keys_phn_1.0.csv')
-tr_label = load_label('../so762_datafiles_new/yuan_tr_labels_phn_1.0.csv')
-phn_dict = gen_phn_dict(tr_label)
-print(phn_dict)
-tr_feat, tr_label = process_feat_seq(tr_feat, tr_keys, tr_label, phn_dict)
-print(tr_feat.shape)
-print(tr_label.shape)
-#print(tr_label[2:])
-np.save('seq_data_new/tr_feat_phn_1.0.npy', tr_feat)
-np.save('seq_data_new/tr_label_phn_1.0.npy', tr_label)
-
-# sequence test data
-te_feat = load_feat('../so762_datafiles_new/yuan_te_feats_phn_1.0.csv')
-te_keys = load_keys('../so762_datafiles_new/yuan_te_keys_phn_1.0.csv')
-te_label = load_label('../so762_datafiles_new/yuan_te_labels_phn_1.0.csv')
-te_feat, te_label = process_feat_seq(te_feat, te_keys, te_label, phn_dict)
-print(te_feat.shape)
-print(te_label.shape)
-np.save('seq_data_new/te_feat_phn_1.0.npy', te_feat)
-np.save('seq_data_new/te_label_phn_1.0.npy', te_label)
-
-# sequence training data
-tr_feat = load_feat('../so762_datafiles_new/yuan_tr_feats_phn_0.1.csv')
+tr_feat = load_feat('../../data/so762_datafiles_new/yuan_tr_feats_phn_0.1.csv')
 tr_keys = load_keys('../so762_datafiles_new/yuan_tr_keys_phn_0.1.csv')
 tr_label = load_label('../so762_datafiles_new/yuan_tr_labels_phn_0.1.csv')
 phn_dict = gen_phn_dict(tr_label)
